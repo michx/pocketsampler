61999872dbe9b08061273bab1d78d33223aa6125
michx <micheleddd@gmail.com>
2020-06-24 11:35:20 +0200

    test1

--- scratch.py
+++ scratch.py
@@ -1,78 +1,51 @@
-#!/usr/bin/sudo python
+#!/usr/bin/python2
 
 
 import argparse
-import datetime
-import signal
+import signal,sys
 import time
+
 
 
 import scapy
 from scapy.all import *
-
-
+from scapy.all import sniff
+from scapy.all import IP
+from scapy.all import DNSQR
+from scapy.all import UDP
+from scapy.all import TCP
+from scapy.all import DNS
+from datetime import datetime
 
 import rb_netflow as rbnf
 
-signal_received = 0
+IP_DST='0.0.0.0'
+PORT_SRC=5000
+PORT_DST=2055
+sampling_rate=5
+buffer_count=10
+records = []  # Array containing records to be sent
+records_in_buffer = 0  # Counts records to be stored before sending them
+packet_count_for_sampling = 0  # Counts packets before sampling occurs
+flow_Sequence=1
 
 
-def preexec():
-    os.setpgrp()  # Don't forward signals
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
 
 
-def signal_handler(signal, frame):
-    global signal_received
-    signal_received = 1
 
-
-def main():
-    if os.getuid() != 0:
-        print "You need to be root to run this, sorry."
-        return
-
-    parser = argparse.ArgumentParser(description='UDP packets producer with scapy')
-    parser.add_argument('-s', '--source-ip', dest='src_ip',
-                        help='IP source')
-    parser.add_argument('-sp', '--source-port', dest='src_port',
-                        help='Port dst')
-    parser.add_argument('-d', '--dst-ip', dest='dst_ip',
-                        help='IP source')
-    parser.add_argument('-dp', '--dst-port', dest='dst_port',
-                        help='Port dst')
-    parser.add_argument('-t', '--time-interval', dest='time_interval',
-                        help='Time interval to wait to send other messages.')
-
-    args = parser.parse_args()
-
-    if args.src_ip:
-        IP_SRC = args.src_ip
-    else:
-        IP_SRC = "10.0.203.2"
-
-    IP_DST = "172.16.27.180"
-
-    if args.src_port:
-        PORT_SRC = int(args.src_port)
-    else:
-        PORT_SRC = int(2056)
-
-    if args.time_interval:
-        TIME_INTERVAL = args.time_interval
-    else:
-        TIME_INTERVAL = 0
-
-    if args.dst_port:
-        PORT_DST = int(args.dst_port)
-    else:
-        PORT_DST = int(2055)
-
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.signal(signal.SIGTERM, signal_handler)
-
+def send_to_collector(records_to_send):
+    global IP_DST,IP_SRC,PORT_SRC,PORT_DST,flow_Sequence
+    s = conf.L2socket()
     # Current timestamp in seconds
-    tnow = (datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).seconds
-
+    tnow = int((datetime.utcnow()-datetime(1970,1,1)).total_seconds())
     # Netflow5
     nfh = rbnf.NetflowHeader(version=5)
     # No need the count field! see rb_netflow.py:post_build
@@ -80,38 +53,128 @@ def main():
         sysUptime=0x3e80, \
         unixSecs=tnow, \
         unixNanoSeconds=0x04bdb6f0, \
-        flowSequence=48, \
-        engineType=0, \
+        flowSequence=flow_Sequence, \
+        engineType=1, \
         engineID=0, \
-        samplingInterval=0)
+        samplingInterval=10)
 
-    # wireshark File -> export specified packet dissections -> as plain text
+    # in case no IP Source is provided leave it blank, so send will detect..
+    try:
+        data = IP(src=IP_SRC,dst=IP_DST) / UDP(sport=PORT_SRC,dport=PORT_DST) / nfh / nf5h
+    except:
+        data = IP(dst=IP_DST) / UDP(sport=PORT_SRC, dport=PORT_DST) / nfh / nf5h
 
-    records = [
-        rbnf.NetflowRecordV5( \
-            src="192.168.0.17", dst="8.8.8.8", nexthop="0.0.0.0", \
-            input=0, output=0, dpkts=1, dOctets=72, \
-            first=1, last=2, srcport=49622, \
-            dstport=53, pad1=0, tcpFlags=0x00, \
-            prot=17, tos=0x00, src_as=0, dst_as=0, \
-            src_mask=0, dst_mask=0, pad2=0),
-    ]
-
-    data = IP(dst=IP_DST) / UDP(dport=PORT_DST) / nfh / nf5h
-    for r in records:
+    #print data[0].show()
+    for r in records_to_send:
         data /= r
+    sys.stdout.write('.')
+    s.send(data)
+    flow_Sequence+=1
 
-    wrpcap('5.pcap', data)
+def process(pkt):
+    global records,sampling_rate,buffer_count,records_in_buffer,packet_count_for_sampling
+    if pkt.haslayer(DNSQR) and UDP in pkt and pkt[UDP].sport == 53:
+        for i in range(0, pkt[DNS].ancount):
+            name = pkt[DNS].an[i].rrname
+            solved = pkt[DNS].an[i].rdata
+            tpe = pkt[DNS].an[i].type
+            now = datetime.now()
+            data = {'Domain': name, 'IP': solved, 'Type': tpe, 'occurrence': 1, 'Last_seen': datetime.now()}
+            #print name, '   ', solved, '  ', tpe
+    packet_count_for_sampling+=1
+    if packet_count_for_sampling==sampling_rate:
+        if TCP in pkt:
+            flgs=pkt[TCP].flags
+        else:
+            flgs=''
+        records.append(
+            rbnf.NetflowRecordV5( \
+                src=pkt[IP].src, dst=pkt[IP].dst, nexthop="192.168.1.1", \
+                input=100, output=200, dpkts=1, dOctets=pkt[IP].len, \
+                first=1, last=2, srcport=pkt[IP].sport, \
+                dstport=pkt[IP].dport, pad1=0, tcpFlags=flgs, \
+                prot=pkt[IP].proto , tos=0x00, src_as=0, dst_as=0, \
+                src_mask=0, dst_mask=0, pad2=0),
+        )
+        records_in_buffer+=1
+        if records_in_buffer==int(buffer_count):
+            send_to_collector(records)
+            records_in_buffer=0
+            records=[]
+        packet_count_for_sampling=0
 
-    send(data)
 
-    while TIME_INTERVAL is not 0:
-        if signal_received == 1:
-            print "\nSignal received. Stopping and Exitting..."
-            sys.exit(0)
-        time.sleep(float(TIME_INTERVAL))
-        send(data)
+def main():
+    global IP_DST,IP_SRC,PORT_SRC,PORT_DST,sampling_rate,buffer_count
+    if os.getuid() != 0:
+        print "You need to be root to run this, sorry."
+        return
+
+    parser = argparse.ArgumentParser(description='Netflow generator for laptops')
+    parser.add_argument('-i', '--interface', dest='int',
+                        help='Monitored interface')
+    parser.add_argument('-s', '--source', dest='src_ip',
+                        help='Source IP address. Used to send packets to collector')
+    parser.add_argument('-sp', '--sport', dest='src_port',
+                        help='Source port. Used to send packets to collector. Default 5000')
+    parser.add_argument('-d', '--destination', dest='dst_ip',
+                        help='Destination IP address. Used to send packets to collector')
+    parser.add_argument('-dp', '--dport', dest='dst_port',
+                        help='Destination port. Used to send packets to collector. Default 2055')
+    parser.add_argument('-r', '--rate', dest='sampling_rate',
+                        help='Sampling 1 out of r packets. Default 5')
+    parser.add_argument('-b', '--buffer', dest='buffer_count',
+                        help='Number of packets stored in buffer before sending them. Default 10')
+
+    args = parser.parse_args()
+
+    if not args.src_ip:
+        print "Source IP Address not provided....trying to get interface IP Address"
+    else:
+        IP_SRC=args.src_ip
+    if not args.dst_ip:
+        print "Destination IP Address is mandatory. Aborting..."
+        exit()
+    else:
+        IP_DST=args.dst_ip
+    if not args.int:
+        print "Monitored interface is mandatory. Aborting..."
+        exit()
+    else:
+        IP_INT=args.int
+    if not args.src_port:
+        PORT_SRC=int(5000)
+    else:
+        PORT_SRC=int(args.src_port)
+    if not args.dst_port:
+        PORT_DST=int(2055)
+    else:
+        PORT_DST=int(args.dst_port)
+
+    if args.dst_port:
+        PORT_DST = int(args.dst_port)
+    else:
+        PORT_DST = int(2055)
+
+    if not args.sampling_rate:
+        sampling_rate=5
+    else:
+        sampling_rate=int(args.sampling_rate)
+
+    if not args.buffer_count:
+        buffer_count=int(10)
+    else:
+        buffer_count=int(args.buffer_count)
+
+
+
+
+    sn=AsyncSniffer(iface=IP_INT,filter='ip and (udp or tcp)', prn=process, store=0)
+    sn.start()
 
 
 if __name__ == '__main__':
-    main()
+    killer = GracefulKiller()
+    while not killer.kill_now:
+        time.sleep(1)
+        main()
